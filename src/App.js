c39ae0470f7d25a88b5ed7b515ec58429ee0ea23
Giancarlo Coppola <gcoppolaa98@gmail.com>
2025-05-17 23:23:52 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const items = [
-  { id: '#22', name: 'Custom Hero', price: 0 },
+  { id: '#22', name: 'Custom', price: 0 },
   { id: '#1', name: '#1', price: 16, category: 'Regular Heroes' },
   { id: '#2', name: '#2', price: 19, category: 'Regular Heroes' },
   { id: '#3', name: '#3', price: 20, category: 'Regular Heroes' },
@@ -40,106 +40,106 @@ const items = [
   { id: '#37', name: '#37', price: 16, category: 'Regular Heroes' },
   { id: '#38', name: '#38', price: 16, category: 'Regular Heroes' },
   { id: '#39', name: '#39', price: 14, category: 'Regular Heroes' },
-  { id: '#40', name: '#40', price: 14 },
-  { id: '#41', name: '#41', price: 21 },
-  { id: '#42', name: '#42', price: 19 },
-  { id: '#43', name: '#43', price: 19 },
-  { id: '#44', name: '#44', price: 18 },
-  { id: '#45', name: '#45', price: 15 },
-  { id: '#46', name: '#46', price: 20 },
-  { id: '#47', name: '#47', price: 27 },
-  { id: '#48', name: '#48', price: 19 },
-  { id: '#49', name: '#49', price: 16 },
-  { id: '#50', name: '#50', price: 18 },
-  { id: '#51', name: '#51', price: 14 },
-  { id: '#52', name: '#52', price: 18 },
-  { id: '#53', name: '#53', price: 16 },
-  { id: '#54', name: '#54', price: 20 },
-  { id: '#55', name: '#55', price: 21 },
-  { id: '#56', name: '#56', price: 16 },
-  { id: '#57', name: '#57', price: 20 },
-  { id: '#58', name: '#58', price: 16 },
-  { id: '#59', name: '#59', price: 23 },
-  { id: '#60', name: '#60', price: 18 },
-  { id: '#61', name: '#61', price: 20 },
-  { id: '#62', name: '#62', price: 22 },
-  { id: '#63', name: '#63', price: 21 },
-  { id: '#64', name: '#64', price: 19 },
-  { id: '#65', name: '#65', price: 20 },
-  { id: '#66', name: '#66', price: 20 },
-  { id: '#67', name: '#67', price: 16 },
-  { id: '#68', name: '#68', price: 19 },
-  { id: '#69', name: '#69', price: 25 },
-  { id: '#70', name: '#70', price: 18 },
-  { id: '#71', name: '#71', price: 20 },
-  { id: '#72', name: '#72', price: 16 },
-  { id: '#73', name: '#73', price: 18 },
-  { id: '#74', name: '#74', price: 16 },
-  { id: '#75', name: '#75', price: 18 },
-  { id: '#76', name: '#76', price: 21 },
-  { id: '#77', name: '#77', price: 16 },
-  { id: '#78', name: '#78', price: 20 },
-  { id: '#79', name: '#79', price: 15 },
-  { id: '#80', name: '#80', price: 22 },
-  { id: '#81', name: '#81', price: 25 },
-  { id: '#82', name: '#82', price: 18 },
-  { id: '#83', name: '#83', price: 15 },
-  { id: '#84', name: '#84', price: 16 },
-  { id: '#85', name: '#85', price: 20 },
-  { id: '#86', name: '#86', price: 16 },
-  { id: '#87', name: '#87', price: 20 },
-  { id: '#88', name: '#88', price: 18 },
-  { id: '#89', name: '#89', price: 18 },
-  { id: '#90', name: '#90', price: 20 },
-  { id: '#91', name: '#91', price: 18 },
-  { id: '#92', name: '#92', price: 18 },
-  { id: '#93', name: '#93', price: 18 },
-  { id: '#94', name: '#94', price: 25 },
-  { id: '#95', name: '#95', price: 23 },
-  { id: '#96', name: '#96', price: 20 },
-  { id: '#97', name: '#97', price: 19 },
-  { id: '#98', name: '#98', price: 25 },
-  { id: '#99', name: '#99', price: 21 },
-  { id: '#100', name: '#100', price: 18 },
-  { id: '#101', name: '#101', price: 20 },
-  { id: '#102', name: '#102', price: 25 },
-  { id: '#103', name: '#103', price: 23 },
-  { id: '#375', name: '#375', price: 19 },
-  { id: '#105', name: '#105', price: 18 },
-  { id: '#106', name: '#106', price: 27 },
-  { id: '#107', name: '#107', price: 25 },
-  { id: '#108', name: '#108', price: 28 },
-  { id: '#109', name: '#109', price: 21 },
-  { id: '#110', name: '#110', price: 23 },
-  { id: '#111', name: '#111', price: 25 },
-  { id: '#112', name: '#112', price: 20 },
-  { id: '#113', name: '#113', price: 24 },
-  { id: '#114', name: '#114', price: 22 },
-  { id: '#115', name: '#115', price: 29 },
-  { id: '#116', name: '#116', price: 25 },
-  { id: '#117', name: '#117', price: 22 },
-  { id: '#118', name: '#118', price: 25 },
-  { id: '#119', name: '#119', price: 29 },
-  { id: '#120', name: '#120', price: 20 },
-  { id: '#121', name: '#121', price: 22 },
-  { id: '#122', name: '#122', price: 24 },
-  { id: '#123', name: '#123', price: 21 },
-  { id: '#124', name: '#124', price: 23 },
-  { id: '#125', name: '#125', price: 28 },
-  { id: '#126', name: '#126', price: 19 },
-  { id: '#127', name: '#127', price: 21 },
-  { id: '#128', name: '#128', price: 25 },
-  { id: '#129', name: '#129', price: 25 },
-  { id: '#130', name: '#130', price: 20 },
-  { id: '#131', name: '#131', price: 21 },
-  { id: '#132', name: '#132', price: 27 },
-  { id: '#133', name: '#133', price: 26 },
-  { id: '#134', name: '#134', price: 18 },
-  { id: '#135', name: '#135', price: 21 },
-  { id: '#136', name: '#136', price: 23 },
-  { id: '#137', name: '#137', price: 25 },
-  { id: '#138', name: '#138', price: 26 },
-  { id: '#139', name: '#139', price: 25 },
+   { id: '#40', name: '#40', price: 14, category: 'Regular Heroes' },
+  { id: '#41', name: '#41', price: 21, category: 'Regular Heroes' },
+  { id: '#42', name: '#42', price: 19, category: 'Regular Heroes' },
+  { id: '#43', name: '#43', price: 19, category: 'Regular Heroes' },
+  { id: '#44', name: '#44', price: 18, category: 'Regular Heroes' },
+  { id: '#45', name: '#45', price: 15, category: 'Regular Heroes' },
+  { id: '#46', name: '#46', price: 20, category: 'Regular Heroes' },
+  { id: '#47', name: '#47', price: 27, category: 'Regular Heroes' },
+  { id: '#48', name: '#48', price: 19, category: 'Regular Heroes' },
+  { id: '#49', name: '#49', price: 16, category: 'Regular Heroes' },
+  { id: '#50', name: '#50', price: 18, category: 'Regular Heroes' },
+  { id: '#51', name: '#51', price: 14, category: 'Regular Heroes' },
+  { id: '#52', name: '#52', price: 18, category: 'Regular Heroes' },
+  { id: '#53', name: '#53', price: 16, category: 'Regular Heroes' },
+  { id: '#54', name: '#54', price: 20, category: 'Regular Heroes' },
+  { id: '#55', name: '#55', price: 21, category: 'Regular Heroes' },
+  { id: '#56', name: '#56', price: 16, category: 'Regular Heroes' },
+  { id: '#57', name: '#57', price: 20, category: 'Regular Heroes' },
+  { id: '#58', name: '#58', price: 16, category: 'Regular Heroes' },
+  { id: '#59', name: '#59', price: 23, category: 'Regular Heroes' },
+  { id: '#60', name: '#60', price: 18, category: 'Regular Heroes' },
+  { id: '#61', name: '#61', price: 20, category: 'Regular Heroes' },
+  { id: '#62', name: '#62', price: 22, category: 'Regular Heroes' },
+  { id: '#63', name: '#63', price: 21, category: 'Regular Heroes' },
+  { id: '#64', name: '#64', price: 19, category: 'Regular Heroes' },
+  { id: '#65', name: '#65', price: 20, category: 'Regular Heroes' },
+  { id: '#66', name: '#66', price: 20, category: 'Regular Heroes' },
+  { id: '#67', name: '#67', price: 16, category: 'Regular Heroes' },
+  { id: '#68', name: '#68', price: 19, category: 'Regular Heroes' },
+  { id: '#69', name: '#69', price: 25, category: 'Regular Heroes' },
+  { id: '#70', name: '#70', price: 18, category: 'Regular Heroes' },
+  { id: '#71', name: '#71', price: 20, category: 'Regular Heroes' },
+  { id: '#72', name: '#72', price: 16, category: 'Regular Heroes' },
+  { id: '#73', name: '#73', price: 18, category: 'Regular Heroes' },
+  { id: '#74', name: '#74', price: 16, category: 'Regular Heroes' },
+  { id: '#75', name: '#75', price: 18, category: 'Regular Heroes' },
+  { id: '#76', name: '#76', price: 21, category: 'Regular Heroes' },
+  { id: '#77', name: '#77', price: 16, category: 'Regular Heroes' },
+  { id: '#78', name: '#78', price: 20, category: 'Regular Heroes' },
+  { id: '#79', name: '#79', price: 15, category: 'Regular Heroes' },
+  { id: '#80', name: '#80', price: 22, category: 'Regular Heroes' },
+  { id: '#81', name: '#81', price: 25, category: 'Regular Heroes' },
+  { id: '#82', name: '#82', price: 18, category: 'Regular Heroes' },
+  { id: '#83', name: '#83', price: 15, category: 'Regular Heroes' },
+  { id: '#84', name: '#84', price: 16, category: 'Regular Heroes' },
+ { id: '#85', name: '#85', price: 20, category: 'Regular Heroes' },
+  { id: '#86', name: '#86', price: 16, category: 'Regular Heroes' },
+  { id: '#87', name: '#87', price: 20, category: 'Regular Heroes' },
+  { id: '#88', name: '#88', price: 18, category: 'Regular Heroes' },
+  { id: '#89', name: '#89', price: 18, category: 'Regular Heroes' },
+  { id: '#90', name: '#90', price: 20, category: 'Regular Heroes' },
+  { id: '#91', name: '#91', price: 18, category: 'Regular Heroes' },
+  { id: '#92', name: '#92', price: 18, category: 'Regular Heroes' },
+  { id: '#93', name: '#93', price: 18, category: 'Regular Heroes' },
+  { id: '#94', name: '#94', price: 25, category: 'Regular Heroes' },
+  { id: '#95', name: '#95', price: 23, category: 'Regular Heroes' },
+  { id: '#96', name: '#96', price: 20, category: 'Regular Heroes' },
+  { id: '#97', name: '#97', price: 19, category: 'Regular Heroes' },
+  { id: '#98', name: '#98', price: 25, category: 'Regular Heroes' },
+  { id: '#99', name: '#99', price: 21, category: 'Regular Heroes' },
+  { id: '#100', name: '#100', price: 18, category: 'Regular Heroes' },
+  { id: '#101', name: '#101', price: 20, category: 'Regular Heroes' },
+  { id: '#102', name: '#102', price: 25, category: 'Regular Heroes' },
+  { id: '#103', name: '#103', price: 23, category: 'Regular Heroes' },
+  { id: '#375', name: '#375', price: 19, category: 'Regular Heroes' },
+  { id: '#105', name: '#105', price: 18, category: 'Regular Heroes' },
+  { id: '#106', name: '#106', price: 27, category: 'Regular Heroes' },
+  { id: '#107', name: '#107', price: 25, category: 'Regular Heroes' },
+  { id: '#108', name: '#108', price: 28, category: 'Regular Heroes' },
+  { id: '#109', name: '#109', price: 21, category: 'Regular Heroes' },
+  { id: '#110', name: '#110', price: 23, category: 'Regular Heroes' },
+  { id: '#111', name: '#111', price: 25, category: 'Regular Heroes' },
+  { id: '#112', name: '#112', price: 20, category: 'Regular Heroes' },
+  { id: '#113', name: '#113', price: 24, category: 'Regular Heroes' },
+  { id: '#114', name: '#114', price: 22, category: 'Regular Heroes' },
+  { id: '#115', name: '#115', price: 29, category: 'Regular Heroes' },
+  { id: '#116', name: '#116', price: 25, category: 'Regular Heroes' },
+  { id: '#117', name: '#117', price: 22, category: 'Regular Heroes' },
+  { id: '#118', name: '#118', price: 25, category: 'Regular Heroes' },
+  { id: '#119', name: '#119', price: 29, category: 'Regular Heroes' },
+  { id: '#120', name: '#120', price: 20, category: 'Regular Heroes' },
+  { id: '#121', name: '#121', price: 22, category: 'Regular Heroes' },
+  { id: '#122', name: '#122', price: 24, category: 'Regular Heroes' },
+  { id: '#123', name: '#123', price: 21, category: 'Regular Heroes' },
+  { id: '#124', name: '#124', price: 23, category: 'Regular Heroes' },
+  { id: '#125', name: '#125', price: 28, category: 'Regular Heroes' },
+  { id: '#126', name: '#126', price: 19, category: 'Regular Heroes' },
+  { id: '#127', name: '#127', price: 21, category: 'Regular Heroes' },
+  { id: '#128', name: '#128', price: 25, category: 'Regular Heroes' },
+  { id: '#129', name: '#129', price: 25, category: 'Regular Heroes' },
+  { id: '#130', name: '#130', price: 20, category: 'Regular Heroes' },
+  { id: '#131', name: '#131', price: 21, category: 'Regular Heroes' },
+  { id: '#132', name: '#132', price: 27, category: 'Regular Heroes' },
+  { id: '#133', name: '#133', price: 26, category: 'Regular Heroes' },
+  { id: '#134', name: '#134', price: 18, category: 'Regular Heroes' },
+  { id: '#135', name: '#135', price: 21, category: 'Regular Heroes' },
+  { id: '#136', name: '#136', price: 23, category: 'Regular Heroes' },
+  { id: '#137', name: '#137', price: 25, category: 'Regular Heroes' },
+  { id: '#138', name: '#138', price: 26, category: 'Regular Heroes' },
+  { id: '#139', name: '#139', price: 25, category: 'Regular Heroes' },
   { id: '#1A', name: '#1A', price: 19, category: 'Smoked Mozzarella Heroes' },
   { id: '#2A', name: '#2A', price: 19, category: 'Smoked Mozzarella Heroes' },
   { id: '#3A', name: '#3A', price: 22, category: 'Smoked Mozzarella Heroes' },
@@ -211,7 +211,7 @@ function App() {
     <div
       style={{
         padding: 20,
-        maxWidth: '1200px',
+        maxWidth: '100%',
         margin: '0 auto',
         fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
         backgroundColor: '#fafafa',
@@ -246,7 +246,7 @@ function App() {
           marginBottom: 20,
         }}
       >
-        <h1 style={{ color: '#333', margin: 0 }}>Hero Sandwich POS</h1>
+        <h1 style={{ color: '#333', margin: 0 }}>Giancarlo's POS</h1>
         <div
           style={{
             position: 'relative',
@@ -283,7 +283,7 @@ function App() {
       <div style={{ marginBottom: 12 }}>
         <input
           type="text"
-          placeholder="Search by name or ID..."
+          placeholder="Search Items..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
           style={{
@@ -378,7 +378,7 @@ function App() {
         }}
         onClick={() => setIsCartOpen(true)}
       >
-        View Cart (${finalTotal.toFixed(2)})
+        View Cart (${subtotal.toFixed(2)})
       </div>
 
       {isCartOpen && (
@@ -413,6 +413,7 @@ function App() {
             }}
             onClick={(e) => e.stopPropagation()}
           >
+          <button onClick={clearCart}>Clear Cart</button>
             <h2>Cart</h2>
             {cart.map((item, index) => (
               <div key={index} style={{ marginBottom: 10 }}>
@@ -442,11 +443,13 @@ function App() {
               </label>
             </div>
             <div style={{ marginTop: 10 }}>
-              <p>Subtotal: ${subtotal.toFixed(2)}</p>
-              <p>Tax: ${tax.toFixed(2)}</p>
-              <p>Service Fee: ${serviceFee.toFixed(2)}</p>
-              <p>Total: ${finalTotal.toFixed(2)}</p>
-              <button onClick={clearCart}>Clear Cart</button>
+              <p><strong>Subtotal: ${subtotal.toFixed(2)}</strong></p>
+              <p>Tip: ${tip.toFixed(2)}</p>
+              <p><strong>Subtotal with Tip: ${subtotalWithTip.toFixed(2)}</strong></p>
+              <p>Sales Tax (8.875%): ${tax.toFixed(2)}</p>
+              <p><strong>Subtotal with Tax: ${subtotalWithTax.toFixed(2)}</strong></p>
+              <p>Service Fee (3.95%): ${serviceFee.toFixed(2)}</p>
+              <p><strong>Final Total: ${finalTotal.toFixed(2)}</strong></p>
             </div>
           </div>
         </div>
